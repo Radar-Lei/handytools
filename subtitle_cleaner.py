a3f278c5ac43b3898cc155093492ce1573a709f0
Radar-Lei <greatradar@gmail.com>
2018-12-14 10:30:07 -0500

    fix greek ahlphabet decode promblem

--- subtitle_cleaner.py
+++ subtitle_cleaner.py
@@ -9,7 +9,7 @@ sentence = re.compile('^.+$')
 
 file_list = glob.glob(os.getcwd()+"/*.txt")
 for each_one in file_list:
-    txt_file = open(each_one, encoding="raw_unicode_escape")
+    txt_file = open(each_one, encoding="utf-8")
     a = txt_file.read()
 
     a = a.replace('\n', '')
