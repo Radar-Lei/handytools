af279c186c2d5de56f695e5a126851a172939f02
Radar-Lei <greatradar@gmail.com>
2018-12-14 00:49:37 -0500

    decode problem solved

--- subtitle_cleaner.py
+++ subtitle_cleaner.py
@@ -9,10 +9,12 @@ sentence = re.compile('^.+$')
 
 file_list = glob.glob(os.getcwd()+"/*.txt")
 for each_one in file_list:
-    txt_file = open(each_one)
+    txt_file = open(each_one, encoding="raw_unicode_escape")
     a = txt_file.read()
+
     a = a.replace('\n', '')
-    out_file = open('1_'+os.path.basename(txt_file.name), 'w')
+    out_file = open('1_'+os.path.basename(txt_file.name),
+                    'w', encoding='utf-8')
     out_file.write(re.sub(r'(?<=[.])(?=[^\s])', r'\n', a))
     out_file.close()
     txt_file.close()
