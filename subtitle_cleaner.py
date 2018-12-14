3b9dc5bb7594841f1b0f51a95f190630bcf2c95a
Radar-Lei <greatradar@gmail.com>
2018-12-14 10:35:40 -0500

    functionalize

--- subtitle_cleaner.py
+++ subtitle_cleaner.py
@@ -5,16 +5,19 @@ import os
 import sys
 import glob
 
-sentence = re.compile('^.+$')
 
-file_list = glob.glob(os.getcwd()+"/*.txt")
-for each_one in file_list:
-    txt_file = open(each_one, encoding="utf-8")
-    a = txt_file.read()
+def main():
+    file_list = glob.glob(os.getcwd()+"/*.txt")
+    for each_one in file_list:
+        txt_file = open(each_one, encoding="utf-8")
+        a = txt_file.read()
+        a = a.replace('\n', '')
+        out_file = open('1_'+os.path.basename(txt_file.name),
+                        'w', encoding='utf-8')
+        out_file.write(re.sub(r'(?<=[.])(?=[^\s])', r'\n', a))
+        out_file.close()
+        txt_file.close()
 
-    a = a.replace('\n', '')
-    out_file = open('1_'+os.path.basename(txt_file.name),
-                    'w', encoding='utf-8')
-    out_file.write(re.sub(r'(?<=[.])(?=[^\s])', r'\n', a))
-    out_file.close()
-    txt_file.close()
+
+if __name__ == "__main__":
+    main()
