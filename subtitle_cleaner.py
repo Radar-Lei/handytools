80ba5e3b1983b7758238fcf4029824a88120c9ca
Radar-Lei <greatradar@gmail.com>
2018-12-14 00:07:04 -0500

    subtitle_cleaner initialize

---
+++ subtitle_cleaner.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# --*-- coding:utf-8 --*--
+import re
+import os
+import sys
+import glob
+
+sentence = re.compile('^.+$')
+
+file_list = glob.glob(os.getcwd()+"/*.txt")
+for each_one in file_list:
+    txt_file = open(each_one)
+    a = txt_file.read()
+    a = a.replace('\n', '')
+    out_file = open('1_'+os.path.basename(txt_file.name), 'w')
+    out_file.write(re.sub(r'(?<=[.])(?=[^\s])', r'\n', a))
+    out_file.close()
+    txt_file.close()
